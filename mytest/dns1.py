71267e2325987df25e283c6dfecebe0249f93f72
yijiaotu <276785957@qq.com>
2016-10-11 11:28:25 +0800

    我的练习

--- mytest/dns1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import dns.resolver
-
-answers = dns.resolver.query('baidu.com', 'A')
-for rdata in answers.response.answer:
-    print rdata
-    for i in rdata.items:
-       print i.address
